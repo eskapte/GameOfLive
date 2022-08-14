568d443ea55f8c97dddd0fb322fcc49054769502
eskapte <eskapte@mail.ru>
2022-08-14 16:23:35 +0300

    Добавьте файлы проекта.

---
+++ GameOfLive/GameField.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLive
+{
+    internal class GameField : IEnumerable<string>
+    {
+        private const string LIVE = "*";
+        private const string SPACE = " ";
+
+        private string[,] _gameField;
+
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        public string this[int row, int col]
+        {
+            get
+            {
+                if (col < 0) col = Width - 1;
+                if (row < 0) row = Height - 1;
+                if (col >= Width) col = 0;
+                if (row >= Height) row = 0;
+
+                return _gameField[row, col];
+            }
+
+            private set
+            {
+                if (col < 0) col = Width - 1;
+                if (row < 0) col = Height - 1;
+                if (col >= Width) col = 0;
+                if (row >= Height) row = 0;
+
+                _gameField[row, col] = value;
+            }
+        }
+
+        public string this[int row]
+        {
+            get
+            {
+                if (row < 0) row = Height - 1;
+                if (row >= Height) row = 0;
+
+                var stringBuilder = new StringBuilder(Width);
+
+                for (int col = 0; col < Width; col++)
+                {
+                    stringBuilder.Append(this[row, col]);
+                }
+
+                return stringBuilder.ToString();
+            }
+        }
+
+        public GameField(int height, int width)
+        {
+            if (height < 1)
+                throw new Exception("Game field height must be greater 0");
+            if (width < 1)
+                throw new Exception("Game field width must be greater 0");
+
+            Width = width;
+            Height = height;
+
+            _gameField = new string[height, width];
+
+            InitGameField();
+        }
+
+        public void SetInitialLive(IEnumerable<Tuple<int, int>> livingCells)
+        {
+            foreach (var cell in livingCells)
+            {
+                this[cell.Item1, cell.Item2] = LIVE;
+            }
+        }
+
+        public void NewTick()
+        {
+            var newField = new string[Height, Width];
+
+            for (int row = 0; row < Height; row++)
+            {
+                for (int col = 0; col < Width; col++)
+                {
+                    var livingNear = GetNearLivingCellsCount(row, col);
+
+                    newField[row, col] = livingNear switch
+                    {
+                        2 => this[row, col],
+                        3 => LIVE,
+                        _ => SPACE
+                    };
+                }
+            }
+
+            _gameField = newField;
+        }
+
+        private int GetNearLivingCellsCount(int row, int col)
+        {
+            var count = 0;
+
+            for (var posY = row - 1; posY <= row + 1; posY++)
+            {
+                for (var posX = col - 1; posX <= col + 1; posX++)
+                {
+                    if (posX == col && posY == row)
+                        continue;
+
+                    if (this[posY, posX] == LIVE)
+                        count++;
+                }
+            }
+
+            return count;
+        }
+
+        private void InitGameField()
+        {
+            for (int row = 0; row < Height; row++)
+            {
+                for (int col = 0; col < Width; col++)
+                {
+                    this[row, col] = SPACE;
+                }
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            for (int row = 0; row < Height; row++)
+            {
+                yield return this[row];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ GameOfLive/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace GameOfLive
+{
+    internal class Program
+    {
+        private const int WIDTH = 80;
+        private const int HEIGHT = 25;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                    throw new Exception("Не передан путь до файла первым аргументом");
+
+                var gameField = new GameField(HEIGHT, WIDTH);
+                var fileInput = ReadInputDataFromFile(args[0]);
+                var livingCells = GetInitialLivingCells(fileInput);
+                gameField.SetInitialLive(livingCells);
+                Render(gameField);
+            }
+            catch (Exception err)
+            {
+                Console.Error.WriteLine(err.Message);
+                return;
+            }
+        }
+
+        private static void Render(GameField gameField)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            while (true)
+            {
+                Console.Clear();
+
+                foreach (var row in gameField)
+                    Console.WriteLine(row);
+
+                gameField.NewTick();
+                Thread.Sleep(300);
+            }
+        }
+
+        private static IEnumerable<Tuple<int, int>> GetInitialLivingCells(IEnumerable<string> data)
+        {
+            var inputFormat = @"^(?<row>\d+) (?<col>\d+\s*$)";
+
+            return data.Select((x, line) =>
+            {
+                var regex = new Regex(inputFormat, RegexOptions.None);
+                var match = regex.Match(x);
+
+                if (!match.Success)
+                    throw new Exception($"Неправильный формат(row, col) на линии {line + 1}: \"{x}\"");
+
+                int row = int.Parse(match.Groups["row"].Value);
+                var col = int.Parse(match.Groups["col"].Value);
+
+                return new Tuple<int, int>(row, col);
+            });
+        }
+
+        private static IEnumerable<string> ReadInputDataFromFile(string filePath)
+        {
+            try
+            {
+                using var reader = File.OpenText(filePath);
+                var lines = reader.ReadToEnd().Split("\n");
+
+                return lines;
+            }
+            catch (Exception err)
+            {
+                throw new Exception($"Ошибка при чтении файла: {err.Message}");
+            }
+        }
+    }
+}
